8d326df79af3ef9390c85787e66cd4244b5a2343
Thanh Nguyen <thanh.nguyen@dirox.net>
2023-04-12 16:48:45 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'managerStaff',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
